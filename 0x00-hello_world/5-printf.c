2058fb23fa39fb4d1c1698054265f15814a37200
Kimnjenga <kimnjenga16@gmail.com>
2022-10-14 04:50:06 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,18 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - function contains main program code
+ * Return: print string; 0 otherwise
+ */
 
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
 
 int main(void)
 {
