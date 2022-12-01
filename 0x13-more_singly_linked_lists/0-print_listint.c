e9fcef4602706e9c2cfe4f978289dc9b76f5dee8
Kimnjenga <kimnjenga16@gmail.com>
2022-12-01 04:16:00 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,13 @@
 size_t print_listint(const listint_t *h)
 {
     size_t count = 0;
+    
     if (h == NULL)
         return (0);
+    
     while (h != NULL)
     {
-        printf(a"%d\n", h->n);
+        printf("%d\n", h->n);
         h=h->next;
         count++;
     }
